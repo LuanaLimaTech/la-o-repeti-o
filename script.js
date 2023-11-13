2c6ab8066a18026d425c89a1bd19698440460cd1
LuanaLimaTech <myluannalyma@gmail.com>
2023-11-13 09:37:58 -0300

    projinicial

---
+++ script.js
@@ -0,0 +1,30 @@
+//FOR
+
+for (let contador =0; contador < 11; contador++){
+    console.log(contador)
+    console.log("Aumentando a contagem!")
+}
+
+let pontosDeVida = 0    
+for(let i = 0; i < 11; i++){
+    pontosDeVida += 1
+    console.log("Tomou a poção mágica " + i)
+}
+
+console.log(pontosDeVida + "Totais!" )
+
+
+//WHILE
+
+let i = 0
+while(i < 5 ){
+    console.log ("Olá!")
+    i++
+}
+
+//DO WHILE
+let contador = 0
+do{
+    console.log("Ola!")
+    contador++
+}while (contador < 4)
